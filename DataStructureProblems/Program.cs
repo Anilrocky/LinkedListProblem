0ce8cd09c7ffcb0f969b125f4ee4838156ea4083
Anilrocky <anilkumarac96@gmail.com>
2023-03-22 14:03:48 +0530

    [Anil] Add:Create Linked list of 56,30,70

--- DataStructureProblems/Program.cs
+++ DataStructureProblems/Program.cs
@@ -5,7 +5,24 @@ namespace DataStructureProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Data structure problem");
+            bool flag = true;
+            LinkedList list = new LinkedList();
+            while (flag)
+            {
+                Console.WriteLine("Choose option to perform \n1.Create Linked list \n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        list.Add(56);
+                        list.Add(30);
+                        list.Add(70);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }                      
         }
     }
 }
